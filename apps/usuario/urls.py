6276f1bf384c8c6a81c1d71a8659e4ea48cb068f
Marcelo <chelo4530>
2022-10-15 19:38:17 -0300

    Añadido editar y eliminar usuarios

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -5,4 +5,6 @@ from .views import *
 urlpatterns = [
     path('listado_usuarios/',login_required(ListadoUsuario.as_view()), name='listar_usuarios'),
     path('registrar_usuario/',login_required(RegistrarUsuario.as_view()), name='registrar_usuario'),
+    path('editar_usuario/<int:pk>/',login_required(ActualizarUsuario.as_view()), name='editar_usuario'),
+    path('eliminar_usuario/<int:pk>/',login_required(EliminarUsuario.as_view()), name='eliminar_usuario'),
 ]
